f197e28141276f08be8116152770d5e0a7478403
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2017-08-05 18:48:08 -0700

    Camera motion

--- Hologram/Assets/Scripts/CameraHandler.cs
+++ Hologram/Assets/Scripts/CameraHandler.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CameraHandler : MonoBehaviour
 {
-	public GameObject objectToFollow;
 	public float speed;
+	public GameObject objectToFollow;
 	public List<Waypoint> waypointList;
-	private int currentWaypointIndex;
 
+	private float lerpTime;
+	private int currentWaypointIndex;
+	private int NextWaypointIndex { get { return currentWaypointIndex < waypointList.Count-1 ? currentWaypointIndex + 1 : waypointList.Count - 1; } }
 	private Waypoint CurrentWaypoint { get { return waypointList[currentWaypointIndex]; } }
+	private Waypoint NextWaypoint { get { return waypointList[NextWaypointIndex]; } }
 	
 	void Awake ()
 	{
@@ -19,6 +22,17 @@ public class CameraHandler : MonoBehaviour
 	
 	void Update ()
 	{
-		
+		lerpTime = Mathf.Min(1.0f, lerpTime + speed * Time.deltaTime);
+
+		if( Vector3.Distance(transform.localPosition, NextWaypoint.position) > 0.01f )
+		{
+			transform.localPosition = Vector3.Lerp(CurrentWaypoint.position, NextWaypoint.position, lerpTime);
+			transform.localRotation = Quaternion.Lerp(Quaternion.Euler(CurrentWaypoint.rotation), Quaternion.Euler(NextWaypoint.rotation), lerpTime);
+		}
+		else
+		{
+			currentWaypointIndex++;
+			lerpTime = 0;
+		}
 	}
 }
